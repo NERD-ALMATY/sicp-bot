c1edd169f13fcee96b83cfb83b0fae663c06dd46
Anvar Umarov <arpanetus@protonmail.com>
2019-10-26 23:25:03 +0600

    Add one failing case for parser.py

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -1,8 +1,12 @@
+import pytest
+
 from sicp_bot import get_parser
-
-
-# TODO: add failing case too
 
 
 def test_get_parser(parse_pattern):
     assert get_parser("(?s).*") == parse_pattern
+
+
+def test_fail_get_parser():
+    with pytest.raises(Exception):
+        assert get_parser("[")
