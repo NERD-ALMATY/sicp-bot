9d55a540e072b61fbc86f45fe101cb7dac97db50
yernar <ernarkazakh07@gmail.com>
2019-11-01 00:26:52 +0600

    added rest services

---
+++ services.py
@@ -0,0 +1,5 @@
+task = {"summary": "Take out trash", "description": "" }
+resp = requests.post('https://todolist.example.com/tasks/', json=task)
+if resp.status_code != 201:
+    raise ApiError('POST /tasks/ {}'.format(resp.status_code))
+print('Created task. ID: {}'.format(resp.json()["id"]))
