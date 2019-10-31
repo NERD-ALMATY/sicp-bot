ac0dcbefdbe0fe61d5176c9b41cfba3f319ed8ed
yernar <ernarkazakh07@gmail.com>
2019-10-31 23:56:00 +0600

    permuatations

--- utils.py
+++ utils.py
@@ -12,3 +12,13 @@ def window(iterable, size=2):
 
 for win in window('abcdefghijkl', 3):
     print('-'.join(win))
+
+
+from itertools import permutations
+
+n = 8
+cols = range(n)
+for vec in permutations(cols):
+    if n == len(set(vec[i]+i for i in cols)) \
+         == len(set(vec[i]-i for i in cols)):
+        print ( vec )
