2207a57500de99fcd68824c10728850da55fcca9
yernar <ernarkazakh07@gmail.com>
2019-11-01 00:25:19 +0600

    adding render

--- utils.py
+++ utils.py
@@ -22,3 +22,30 @@ for vec in permutations(cols):
     if n == len(set(vec[i]+i for i in cols)) \
          == len(set(vec[i]-i for i in cols)):
         print ( vec )
+
+
+# Examples to render Irene
+class Counter(object):
+
+    def __init__(self, start=0):
+        self.v = start
+
+    def increase(self):
+        self.v += 1
+
+    def decrease(self):
+        self.v -= 1
+
+
+@presentation.render_for(Counter)
+def render(self, h, comp, *args):
+    with h.div(class_='counter'):
+        h << h.div(self.v)
+
+        with h.span:
+            h << h.a('--').action(self.decrease)
+
+        with h.span:
+            h << h.a('++').action(self.increase)
+
+    return h.root
